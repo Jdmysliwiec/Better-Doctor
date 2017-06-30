53a3224a332411e06e548acef3ab81283deb6e93
Jordan <jdmysliwiec@gmail.com>
2017-06-30 16:16:33 -0700

    add more data displayed when search results are returned. Add small amount of styling with sass.

--- js/better_doctor-interface.js
+++ js/better_doctor-interface.js
@@ -6,10 +6,18 @@ var displayDoctors = function(doctorArray) {
     $('.show-doctors').append(
       '<div class="doctor">' +
       '<h2>' + doctorArray[i].profile.first_name + ' ' + doctorArray[i].profile.last_name + '</h2>' +
+      '<img src=' + doctorArray[i].profile.image_url + '>' +
+      '<ul class="properties">' +
+        '<li> Specialty: ' + doctorArray[i].specialties[0].actor + '</li>' +
+        '<li>' + doctorArray[i].specialties[0].description + '</li>' +
+        '<li>' + doctorArray[i].practices[0].name + '</li>' +
+        '<li>' + '<a href="' + doctorArray[i].practices[0].website + '">Practice website</a>' +
+      '</ul>' +
       '</div>'
     );
   }
-}
+};
+
 $ (function() {
   var newDoctorSearch = new DoctorSearch();
   $('#medical-condition').submit(function(event) {
